975acd57b5400d9cd15c92cc1d3b22bdf2721b81
Baz Murphy <bazmurphy@gmail.com>
2022-08-29 12:54:23 +0100

    restyling

--- src/ProjectList.js
+++ src/ProjectList.js
@@ -6,9 +6,58 @@ const ProjectList = () => {
 
     return (
         <div className="projects-container">
+            <Project 
+                moduleName="React"
+                moduleNameImageClass="react"
+                moduleNameImageSrc="./react.png"
+                moduleNameImageAlt="React Logo"
+                classWorkOrCourseWork="Classwork"
+                weekNumber="1-3"
+                syllabusLink="https://syllabus.codeyourfuture.io/react/week-3/lesson"
+                projectName="Pokedex"
+            />
+            <div className="finished-projects-container">
+                <FinishedProject 
+                    finishedProjectName="Pokedex"
+                    finishedProjectLinkLive="https://cyf-bazmurphy-pokedex.netlify.app/"
+                    finishedProjectLinkLiveImageClass="netlify"
+                    finishedProjectLinkLiveImageSrc="./netlify.png"
+                    finishedProjectLinkLiveImageAlt="Netlify Logo"
+                    finishedProjectLinkCode="https://github.com/bazmurphy/react-pokedex"
+                    finishedProjectLinkCodeImageClass="github"
+                    finishedProjectLinkCodeImageSrc="./github-dark.png"
+                    finishedProjectLinkCodeImageAlt="GitHub Logo"
+                />
+            </div>
+            <Project
+                moduleName="React"
+                moduleNameImageClass="react"
+                moduleNameImageSrc="./react.png"
+                moduleNameImageAlt="React Logo"
+                classWorkOrCourseWork="Coursework"
+                weekNumber="1-3"
+                syllabusLink="https://syllabus.codeyourfuture.io/react/week-3/homework#2-cyf-hotel-5-hours"
+                projectName="Hotel"
+            />
+            <div className="finished-projects-container">
+                <FinishedProject 
+                    finishedProjectName="Hotel"
+                    finishedProjectLinkLive="https://cyf-bazmurphy-hotel-react.netlify.app/"
+                    finishedProjectLinkLiveImageClass="netlify"
+                    finishedProjectLinkLiveImageSrc="./netlify.png"
+                    finishedProjectLinkLiveImageAlt="Netlify Logo"
+                    finishedProjectLinkCode="https://github.com/bazmurphy/react-hotel"
+                    finishedProjectLinkCodeImageClass="github"
+                    finishedProjectLinkCodeImageSrc="./github-dark.png"
+                    finishedProjectLinkCodeImageAlt="GitHub Logo"
+                />
+            </div>
             <Project
                 moduleName="Node"
-                lessonOrCoursework="Coursework"
+                moduleNameImageClass="node"
+                moduleNameImageSrc="./node.png"
+                moduleNameImageAlt="Node Logo"
+                classWorkOrCourseWork="Coursework"
                 weekNumber="1"
                 syllabusLink="https://syllabus.codeyourfuture.io/node/week-1/homework#4-quote-server-api-project-10-hours-"
                 projectName="Quote Server"
@@ -39,7 +88,10 @@ const ProjectList = () => {
             </div>
             <Project
                 moduleName="Node"
-                lessonOrCoursework="Coursework"
+                moduleNameImageClass="node"
+                moduleNameImageSrc="./node.png"
+                moduleNameImageAlt="Node Logo"
+                classWorkOrCourseWork="Coursework"
                 weekNumber="2"
                 syllabusLink="https://syllabus.codeyourfuture.io/node/week-2/homework#2-chat-server-api-project-12-hours-"
                 projectName="Chat Server"
@@ -70,7 +122,10 @@ const ProjectList = () => {
             </div>
             <Project
                 moduleName="Node"
-                lessonOrCoursework="Coursework"
+                moduleNameImageClass="node"
+                moduleNameImageSrc="./node.png"
+                moduleNameImageAlt="Node Logo"
+                classWorkOrCourseWork="Coursework"
                 weekNumber="3"
                 syllabusLink="https://syllabus.codeyourfuture.io/node/week-3/homework#2-hotel-server-12-hours-"
                 projectName="Hotel Server"
