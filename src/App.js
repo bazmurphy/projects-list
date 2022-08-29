9fdd2d21099a4e47373a6594de5724eeda0da8a8
Baz Murphy <bazmurphy@gmail.com>
2022-08-29 13:41:49 +0100

    more styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from "./Header";
 import ProjectList from './ProjectList';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <ProjectList />
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <div className="header-container">
+            <img className="header-image" src="./codeyourfuture.png" alt="CodeYourFuture Logo" />
+            <h1 className="header-title">Projects 💻</h1>
+        </div>
+    );
+};
+
+export default Header;
